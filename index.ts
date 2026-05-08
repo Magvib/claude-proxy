e00bf80d2b7789e3d77c2f958b62d9a66fba6e41
Magnus <mbn@ditur.dk>
2026-05-08 12:31:36 +0200

    Init

---
+++ index.ts
@@ -0,0 +1,72 @@
+console.log("\x1b[1;36mStarting claude-proxy...\x1b[0m");
+
+const ollamaKey = "f7bd5d05a69f43b69a778fc8c31d7357.mr6Yiy5QXrQgo-WFxRBJIlKH";
+const port = 1810;
+const opusAlternative = "glm-5.1";
+const sonnetAlternative = "kimi-k2.6";
+const haikuAlternative = "deepseek-v4-flash";
+
+const log = process.argv.includes("--log") || process.argv.includes("-l");
+const logPrefix = log ? "\x1b[1;36m" : "";
+
+if (log) {
+    console.log(`${logPrefix}Starting server on port ${port}...`);
+}
+
+const server = Bun.serve({
+    port: port,
+    routes: {
+        "/v1/models": async req => {
+            const ollamaModels = await fetch("https://ollama.com/v1/models").then(res => res.json()) as { object: string; data: Array<{ id: string; object: string; owned_by: string; created: number }> };
+
+            const claudeModels = {
+                data: ollamaModels.data.map((model) => ({
+                    created_at: new Date(model.created * 1000).toISOString(),
+                    display_name: model.id.replace(/[:-]/g, " ").replace(/\b\w/g, c => c.toUpperCase()),
+                    id: 'opus-' + model.id,
+                    type: "model"
+                }))
+            };
+
+            return new Response(JSON.stringify(claudeModels), {
+                headers: { "Content-Type": "application/json" }
+            });
+        },
+        "/v1/web_search": async req => {
+            
+            
+            return new Response("OK");
+        },
+        
+        "/*" : async req => {
+            const url = new URL(req.url);
+            const path = url.pathname;
+            const method = req.method;
+            var body = await req.text();
+            const model = body.match(/"model":"([^"]+)"/)?.[1] || "";
+
+            // replace claude-opus-4-7 with kimi-k2.6
+            body = body.replace(/"model":"claude-opus-4-7"/g, `"model":"${opusAlternative}"`);
+            body = body.replace(/"model":"claude-sonnet-4-6"/g, `"model":"${sonnetAlternative}"`);
+            body = body.replace(/"model":"claude-haiku-4-5-20251001"/g, `"model":"${haikuAlternative}"`);
+
+            // remove 'opus-'
+            body = body.replace(/"model":"opus-/g, '"model":"');
+
+            const response = await fetch(`https://ollama.com/${path}`, {
+                method,
+                body,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${ollamaKey}`
+                }
+            });
+
+            if (log) {
+                console.log(logPrefix, method, path, response.status, 'model', model);
+            }
+
+            return response;
+        }
+    }
+});
